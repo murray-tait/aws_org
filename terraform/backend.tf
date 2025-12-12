12a5b5e21c406d4cc601bc4c17544fbdc9966025
Murray Tait <6074789+deathtumble@users.noreply.github.com>
2025-12-12 20:03:41 +0000

    Fix Terraform state file key in backend configuration

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket       = "org.murraytait.experiment.build.terraform"
-    key          = "env/aws-org/org.tfstate"
+    key          = "env/aws-org/terraform.tfstate"
     region       = "eu-west-1"
     profile      = "973963482762_TerraformStateAccess"
     use_lockfile = true
